416032bb19362793171b313f3da73421b59c0a0c
JeffersonClynton <jeffersongba0@gmail.com>
2026-02-24 11:37:21 -0300

    Add files via upload

---
+++ src/dataset/informacao.js
@@ -0,0 +1,27 @@
+// Ele armazena objetos, onde cada objeto representa
+// uma fruta que será exibida em formato de card na tela.
+let informacoesCards = [
+    {
+      url: 'https://agroflorestamazonia.com/wp-content/uploads/2023/05/laranja00.jpg',
+      titulo: 'Laranja',
+      subtitulo: 'Rica em vitamina C e muito cultivada na propriedade.',
+    },
+    {
+      url: 'https://th.bing.com/th/id/R.cac57d31e6b056459394c0c41e78f314?rik=aH4M%2faLFBTzK4g&riu=http%3a%2f%2fwww.saudedica.com.br%2fwp-content%2fuploads%2f2014%2f08%2fuva_benef%c3%adcios.jpg&ehk=aI0%2bndX3W6ResNFCs7%2bjevVi0bkN%2bsdf9OsyDXj%2fX6w%3d&risl=&pid=ImgRaw&r=0',
+      titulo: 'Uva',
+      subtitulo: 'Fonte de antioxidantes e excelente para produção.',
+    },
+    {
+      url: 'https://capitalist.com.br/wp-content/uploads/2022/03/manga-frutas-beneficios-9_Easy-Resize.com_.jpg',
+      titulo: 'Manga',
+      subtitulo: 'Alta produtividade e grande aceitação no mercado.',
+    },
+    {
+      url: 'https://jpimg.com.br/uploads/2025/01/7-beneficios-da-maca-para-a-saude-e-formas-criativas-de-usa-la.jpg',
+      titulo: 'Maçã',
+      subtitulo: 'Cultivo estratégico para diversificação da produção.',
+    },
+  ];
+  
+  export default informacoesCards;
+  // export { informacoesCards};
